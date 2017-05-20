d229d2e5195b46b625b0e35773ef62e680c1d741
Daniel <daniel.999@gmx.de>
2017-05-20 11:44:30 +0200

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
 	end
 
 	def new
@@ -13,31 +16,31 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-	  	@article = Article.new(article_params)
-	  	@article.save
+	  @article = Article.new(article_params)
+	  @article.save
 
-	  	flash.notice = "Article #{@article.title} has been created!"
-	  	redirect_to article_path(@article)
+	  flash.notice = "Article #{@article.title} has been created!"
+	  redirect_to article_path(@article)
   	end
 
-  	def destroy
-  		@article = Article.find(params[:id])
-  		@article.destroy
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
 
-  		flash.notice = "Article #{@article.title} has been destroyed!"
-  		redirect_to articles_path
-  	end
+  	flash.notice = "Article #{@article.title} has been destroyed!"
+  	redirect_to articles_path
+  end
 
-  	def edit
-  		@article = Article.find(params[:id])
-  	end
+  def edit
+  	@article = Article.find(params[:id])
+  end
 
-  	def update
-  		@article = Article.find(params[:id])
-  		@article.update(article_params)
+  def update
+  	@article = Article.find(params[:id])
+  	@article.update(article_params)
 
-  		flash.notice = "Article #{@article.title} Updated!"
+  	flash.notice = "Article #{@article.title} Updated!"
 
-  		redirect_to article_path(@article)
-  	end
+  	redirect_to article_path(@article)
+  end
 end
